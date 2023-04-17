6f4c55cc0080013b13f65ea4a12ee35c00cc0897
ArtoriasDelAbismo <jerojournade1@gmail.com>
2023-04-17 18:49:06 -0300

    finish components, containers, pages and styles

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles/global.css";
 import Layout from "../containers/Layout";
-import CreatePassword from "../containers/CreatePassword";
+import CreatePassword from "../pages/CreatePassword";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
-import RecoverPassword from "../containers/RecoverPassword";
-import Login from "../containers/Login";
-import CreateAccount from "../containers/CreateAccount";
-import MyAccount from "../containers/MyAccount";
+import RecoverPassword from "../pages/RecoverPassword";
+import Login from "../pages/Login";
+import CreateAccount from "../pages/CreateAccount";
+import MyAccount from "../pages/MyAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
 const App = () => {
   return (
     <BrowserRouter>
@@ -21,6 +23,8 @@ const App = () => {
         <Route path="/Login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/MyAccount" element={<MyAccount />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/Orders" element={<Orders />} />
       </Routes>
     </BrowserRouter>
   );
