f94205ff7e016a0fdc1387b213fe3921776c2b38
ArtoriasDelAbismo <jerojournade1@gmail.com>
2023-04-18 18:57:06 -0300

    fix bug in header

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles/global.css";
-import Layout from "../containers/Layout";
 import CreatePassword from "../pages/CreatePassword";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
@@ -11,21 +10,25 @@ import CreateAccount from "../pages/CreateAccount";
 import MyAccount from "../pages/MyAccount";
 import Checkout from "../pages/Checkout";
 import Orders from "../pages/Orders";
+import SendEmail from "../pages/SendEmail";
+import Layout from "../containers/Layout";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-password" element={<CreatePassword />} />
-        <Route path="/recover-password" element={<RecoverPassword />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/Layout" element={<Layout />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/MyAccount" element={<MyAccount />} />
-        <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/Orders" element={<Orders />} />
-      </Routes>
+      <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-password" element={<CreatePassword />} />
+          <Route path="/recover-password" element={<RecoverPassword />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/MyAccount" element={<MyAccount />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/Orders" element={<Orders />} />
+          <Route path="/Send-Email" element={<SendEmail />} />
+        </Routes>
+      
     </BrowserRouter>
   );
 };
