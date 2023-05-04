9839bd6e97f3feb1c0d2e5aacd19945fef4d4e4d
ArtoriasDelAbismo <jerojournade1@gmail.com>
2023-05-04 06:51:53 -0300

    get form data with useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,59 @@
-import React from "react";
-import '@styles/Login.scss';
-import logo from '@logos/logo_yard_sale.svg'
+import React, { useRef } from "react";
+import "@styles/Login.scss";
+import logo from "@logos/logo_yard_sale.svg";
 
 const Login = () => {
-    return (
-      <div className="login">
-        <div className="form-container">
-          <img src= {logo} alt="logo" className="logo" />
+  const form = useRef(null);
 
-          <form action="/" className="form">
-            <label for="email" className="label">
-              Email address
-            </label>
-            <input
-              type="text"
-              id="email"
-              placeholder="platzi@example.cm"
-              className="input input-email"
-            />
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-            <label for="password" className="label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="*********"
-              className="input input-password"
-            />
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+  };
+  return (
+    <div className="login">
+      <div className="form-container">
+        <img src={logo} alt="logo" className="logo" />
 
-            <input
-              type="submit"
-              value="Log in"
-              className="primary-button login-button"
-            />
-            <a href="/">Forgot my password</a>
-          </form>
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email" className="label">
+            Email address
+          </label>
+          <input
+            type="text"
+            name="email" //cambiamos el id a 'name' para utilizar el ref
+            placeholder="platzi@example.cm"
+            className="input input-email"
+          />
 
-          <button className="secondary-button signup-button">Sign up</button>
-        </div>
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password" //cambiamos el id a 'name' para utilizar el ref
+            placeholder="*********"
+            className="input input-password"
+          />
+
+          <button
+            className="primary-button login-button"
+            onClick={handleSubmit}
+          >
+            Log in
+          </button>
+          <a href="/">Forgot my password</a>
+        </form>
+
+        <button className="secondary-button signup-button">Sign up</button>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Login;
